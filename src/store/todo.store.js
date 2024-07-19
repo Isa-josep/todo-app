8d52c8d238ebb8de2b19e41d8c2cfb4f83f6da63
isa <isaurini1902@gmail.com>
2024-07-18 21:47:52 -0600

    feat: Implement missing functions in todo.store.js
    The commit adds the missing functions in the `todo.store.js` file to complete the implementation of the todo store.

--- src/store/todo.store.js
+++ src/store/todo.store.js
@@ -24,34 +24,57 @@ const loadStore=()=>{
 }
 
 const getTodos=(filter = Filters.All)=>{
-    throw new Error('Not implemented');
+    switch(filter){
+        case Filters.All:
+            return [...state.todos];
+        
+        case Filters.Completed:
+            return state.todos.filter( todo => todo.done);
+
+        case Filters.Pendind:
+            return state.todos.filter( todo => !todo.done);
+
+        default:
+            throw new Error('Invalid filter');
+    }
 }
 
-const addTodo=(todoId)=>{
-    throw new Error('Not implemented');
+const addTodo=(description)=>{
+    if(!description){
+        throw new Error('Description is required');
+    }
+    state.todos.push(new Todo (description));
 }
 
 const toggleTodo=(todoId)=>{
-    throw new Error('Not implemented');
+
+    state.todos = state.todos.map(todo=>{
+        if(todo.id ===todoId ){
+            todo.done = !todo.done;
+        }
+        return todo;
+    });
+    
 }
 
 const deleteTodo=(todoId)=>{
-    throw new Error('Not implemented');
+    state.todos= state.todos.filter(todo => todo.id !== todoId);
 }
 
 const deleteCompleted=()=>{
-    throw new Error('Not implemented');
+    state.todos= state.todos.filter(todo => todo.done);
 }
 
 const setFilter=(newFilters = Filters.All)=>{
-    throw new Error('Not implemented');
+    state.filter = newFilters;
 }
 
 const getCurrentFilter=()=>{
-    throw new Error('Not implemented');
+    return state.filter;
 }
 
 export default {
+    addTodo,
     deleteCompleted,
     deleteTodo,
     getCurrentFilter,
