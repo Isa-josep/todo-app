16127946702a692643855aeefae2f12fac5d80ed
isa <isaurini1902@gmail.com>
2024-07-22 16:46:24 -0600

    fix: Add event listener to toggle todo completion status
    The code changes in `app.js` add an event listener to the `todoListUL` element to toggle the completion status of a todo item when clicked. This ensures that users can mark todos as complete or incomplete directly from the UI.

--- src/todo/app.js
+++ src/todo/app.js
@@ -25,12 +25,19 @@ export const App =(elementId)=>{
     })();
     //referencias 
     const newDescriptionInput = document.querySelector(ElementIds.NewTodoInput);
+    const todoListUL = document.querySelector(ElementIds.TodoList);
 
     newDescriptionInput.addEventListener('keyup', (event)=>{
         if(event.keyCode!==13) return;
         if(event.target.value.trim().length === 0) return;
         todoStore.addTodo (event.target.value);
         displayTodos();
+        
         event.target.value='';
+    });
+    todoListUL.addEventListener('click',(event)=>{
+        const element =event.target.closest('[data-id]');
+        todoStore.toggleTodo(element.getAttribute('data-id'));
+        displayTodos();
     });
 }
